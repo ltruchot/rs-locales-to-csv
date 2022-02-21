cd4c390bb2d2ee0cbc102b751d2bc2cc217eb045
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2022-02-21 22:20:12 +0100

    :sparkles: feat(main): found JSON contents in **/locales/*.json files

---
+++ README.md
@@ -0,0 +1,1 @@
+# rs-locales-to-csv
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
+use std::path::Path;
+use walkdir::WalkDir;
+use std::fs;
+
+fn main() {
+    for entry in WalkDir::new("./data")
+        .into_iter()
+        .filter(|dir| match dir {
+            Ok(dir_found) => {
+                dir_found.path().parent()
+                .and_then(Path::to_str)
+                .map_or(false, |s| s.contains("locales"))
+            }
+            Err(_) => false,
+        })
+        .flatten()
+    {
+        let d_name = entry.path().parent().and_then(Path::to_str);
+        let f_name = entry.file_name().to_string_lossy();
+        if let Some(path) = d_name {
+            if path.contains("locales") && f_name.ends_with(".json") {
+                println!("{:?}", fs::read_to_string(entry.path()).unwrap_or_default());
+            }
+            
+        }
+    }
+}
